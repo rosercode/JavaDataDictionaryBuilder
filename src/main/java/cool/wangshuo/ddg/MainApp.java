77a5a64a3c3f78bd1028440c99e4c834dcd88b6b
rosercode <rosercode@gmail.com>
2022-11-10 10:33:51 +0800

    chore Init

---
+++ src/main/java/cool/wangshuo/ddg/MainApp.java
@@ -0,0 +1,15 @@
+package cool.wangshuo.ddg;
+
+
+/**
+ * @author wangshuo
+ * @description
+ * @createDate 2022/11/10 10:00
+ */
+
+public class MainApp{
+
+    public static void main(String[] args) {
+
+    }
+}
